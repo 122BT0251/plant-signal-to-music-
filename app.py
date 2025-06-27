0419be36a08f245d465f703c7ec03ae6e49df802
Anu Priya <162083061+122BT0251@users.noreply.github.com>
2025-06-27 11:41:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,3 +137,9 @@ def generate():
 @app.route('/')
 def home():
     return jsonify({"message": "Backend is running"}), 200
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
