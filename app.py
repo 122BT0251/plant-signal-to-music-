2cc1d9096d803ef3cd0c8471df8a5fd696bf47a2
Anu Priya <162083061+122BT0251@users.noreply.github.com>
2025-06-30 16:33:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -135,4 +135,5 @@ def generate_audio():
 
 # --- Run Locally or via Gunicorn ---
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
+
